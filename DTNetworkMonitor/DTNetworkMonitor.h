59551827c2278165cb62c258fdf9234bbb50e40c
Abin Baby <abin0992@gmail.com>
2023-11-09 22:40:13 +0100

    Initial Commit

---
+++ DTNetworkMonitor/DTNetworkMonitor.h
@@ -0,0 +1,18 @@
+//
+//  DTNetworkMonitor.h
+//  DTNetworkMonitor
+//
+//  Created by Abin Baby on 09.11.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DTNetworkMonitor.
+FOUNDATION_EXPORT double DTNetworkMonitorVersionNumber;
+
+//! Project version string for DTNetworkMonitor.
+FOUNDATION_EXPORT const unsigned char DTNetworkMonitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTNetworkMonitor/PublicHeader.h>
+
+
